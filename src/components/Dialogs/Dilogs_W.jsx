ec9fd98ee8ac21d4cf88064c0c2b7c05e4f8b3b9
Администратор <zavodavia@yandex.ru>
2021-06-07 12:12:11 +0800

    anfinisht
    This reverts commit 50753ff9

---
+++ src/components/Dialogs/Dilogs_W.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {addMessageActive, addNewMessageActive} from "../datafile/DilogsPageReduser";
+
+
+let DilogsStore = (state)=>{
+ return{
+     DilogsData:state.dataDialogs
+ }
+}
+
+let DilogsMetods =(action)=>{
+  return{
+
+  }
+
+
+
+}
