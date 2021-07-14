00254a58633d8094207a82f7cfce6a17e19c0fae
jesto2000 <85092206+jesto2000@users.noreply.github.com>
2021-07-14 11:01:13 +0800

    UsresAxios

--- src/components/Users/users.jsx
+++ src/components/Users/users.jsx
@@ -1,45 +1,29 @@
 import React from 'react';
 import h from './users.module.css';
+import * as axios from 'axios';
+import usphorto from '../../../src/resors/images/user.jpg';
 
 
 const Users = (props) => {
+    let getUsers =()=>{
 
     if (props.UsersData.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.getUsers(response.data.items)
+        })
+    }}
 
-        props.getUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                    followed: true,
-                    fullName: 'Dmitry',
-                    status: 'I am a boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                    followed: false,
-                    fullName: 'Dmitry',
-                    status: 'I am a boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                    followed: false,
-                    fullName: 'Dmitry',
-                    status: 'I am a boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                }
+    // props.getUsers([ ])}
 
-            ]
-        )
-    }
+    return <div>
+        <button onClick={getUsers}>Users</button>
+        {props.UsersData.map(u => <div key={u.id}>
 
-    return <div>{
-        props.UsersData.map(u => <div key={u.id}>
-
-            <img src={u.photoUrl} className={h.userPhoto}/>
+            <img src={u.photos.small!==null?u.photos.small:usphorto} className={h.userPhoto}/>
+            <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
             <div>
                 {u.followed ?
                     <button onClick={() => {
@@ -48,6 +32,10 @@ const Users = (props) => {
                     <button onClick={() => {
                         props.followAA(u.id)
                     }}>Follow</button>}
+                <span>
+                        <div>{u.fullName}</div>
+                        <div>{u.status}</div>
+                    </span>
             </div>
 
         </div>)
